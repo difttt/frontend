16145f70d103ce43f8b1d6af40a0430b16da0f04
shiyivei <shiyivei@outlook.com>
2022-08-27 23:27:49 +0800

    add api to interact with dex pallet

--- src/pages/AutoInvest/index.tsx
+++ src/pages/AutoInvest/index.tsx
@@ -1,126 +1,222 @@
-import './index.css'
-import { useState } from 'react'
-import { Button, Form, Input, Menu, Modal, Radio, Select, Space, Table, TimePicker } from 'antd'
-import type { ColumnsType } from 'antd/es/table'
-import extension from '../../substrate/extension'
+import "./index.css";
+import { useState } from "react";
+import {
+  Button,
+  Form,
+  Input,
+  Menu,
+  Modal,
+  Radio,
+  Select,
+  Space,
+  Table,
+  TimePicker,
+} from "antd";
+import type { ColumnsType } from "antd/es/table";
+import extension from "../../substrate/extension";
+import * as index from "../../substrate/index";
+
+async function getChainInfo() {
+  const chainInfo = await index.getChainInfo();
+  console.log(
+    "substrate blockchain connected!!! chain information:",
+    chainInfo
+  );
+}
+
+getChainInfo();
+
+async function getUser(userName: string) {
+  const user = await index.getUser(userName);
+  console.log("address:", user.address);
+}
+
+getUser("Alice");
+getUser("Bob");
 
 interface CreateForm {
-  investCurrency: string
-  amount: string
-  buyCurrency: string
-  cycleType: string // 'day' | 'week' | 'month'
-  cycleDate: string
-  cycleTime: string
-  buyPrice: number
+  investCurrency: string;
+  amount: string;
+  buyCurrency: string;
+  cycleType: string; // 'day' | 'week' | 'month'
+  cycleDate: string;
+  cycleTime: string;
+  buyPrice: number;
 }
 
 function AutoInvest() {
-  console.log(extension.allAccounts, extension.extensions)
-  const extensions = extension.extensions
-  const allAccounts = extension.allAccounts
+  console.log(
+    extension.allAccounts,
+    extension.extensions
+  );
+  const extensions = extension.extensions;
+  const allAccounts = extension.allAccounts;
 
-  const [form] = Form.useForm()
-  const [createModalVisible, setCreateModalVisible] = useState(false)
+  const [form] = Form.useForm();
+  const [
+    createModalVisible,
+    setCreateModalVisible,
+  ] = useState(false);
 
-  const [current, setCurrent] = useState('')
+  const [current, setCurrent] = useState("");
   const handleCreatePlan = (record: DataType) => {
-    console.log('create plan', record.currency)
-    setCurrent(record.currency)
-    setCreateModalVisible(true)
-  }
+    console.log("create plan", record.currency);
+    setCurrent(record.currency);
+    setCreateModalVisible(true);
+  };
 
   interface DataType {
-    key: string
-    currency: string
+    key: string;
+    currency: string;
   }
 
   const columns: ColumnsType<DataType> = [
     {
-      title: 'Currency',
-      dataIndex: 'currency',
-      key: 'currency',
-      align: 'center',
+      title: "Currency",
+      dataIndex: "currency",
+      key: "currency",
+      align: "center",
       render: (text: string) => {
-        return <span style={{ fontWeight: 'bold', fontSize: '16px' }}>{text.toUpperCase()}</span>
+        return (
+          <span
+            style={{
+              fontWeight: "bold",
+              fontSize: "16px",
+            }}
+          >
+            {text.toUpperCase()}
+          </span>
+        );
       },
     },
     {
-      title: 'Action',
-      key: 'action',
-      align: 'center',
+      title: "Action",
+      key: "action",
+      align: "center",
       render: (_, record) => (
         <Space size="middle">
-          <Button type="primary" onClick={handleCreatePlan.bind(null, record)}>Create Plan</Button>
+          <Button
+            type="primary"
+            onClick={handleCreatePlan.bind(
+              null,
+              record
+            )}
+          >
+            Create Plan
+          </Button>
         </Space>
       ),
     },
-  ]
+  ];
 
   const data: DataType[] = [
     {
-      key: '2',
-      currency: 'BTC',
+      key: "2",
+      currency: "BTC",
     },
     {
-      key: '3',
-      currency: 'ETH',
+      key: "3",
+      currency: "ETH",
     },
     {
-      key: '4',
-      currency: 'USDT',
+      key: "4",
+      currency: "USDT",
     },
-  ]
+  ];
 
-  const [currentExtension, setExtension] = useState('')
-  const [currentAccounts, setAccounts] = useState([] as any[])
-  const [currentAccount, setAccount] = useState()
-  const handleExtensionChange = (value: string) => {
-    setExtension(value)
-    const accounts = allAccounts.filter(account => account.meta.source === value) || []
-    setAccounts(accounts)
-  }
+  const [currentExtension, setExtension] =
+    useState("");
+  const [currentAccounts, setAccounts] = useState(
+    [] as any[]
+  );
+  const [currentAccount, setAccount] = useState();
+  const handleExtensionChange = (
+    value: string
+  ) => {
+    setExtension(value);
+    const accounts =
+      allAccounts.filter(
+        (account) => account.meta.source === value
+      ) || [];
+    setAccounts(accounts);
+  };
   return (
     <div>
       <div className="header">
         <h1>Red Stone</h1>
-        <div style={{ position: 'absolute', right: '20px' }}>
-          <Select value={currentExtension} style={{ width: 120, marginRight: '20px' }} onChange={ handleExtensionChange }>
-            {extensions.map(extension => (
-              <Select.Option key={extension.name} value={extension.name}>{extension.name}</Select.Option>
+        <div
+          style={{
+            position: "absolute",
+            right: "20px",
+          }}
+        >
+          <Select
+            value={currentExtension}
+            style={{
+              width: 120,
+              marginRight: "20px",
+            }}
+            onChange={handleExtensionChange}
+          >
+            {extensions.map((extension) => (
+              <Select.Option
+                key={extension.name}
+                value={extension.name}
+              >
+                {extension.name}
+              </Select.Option>
             ))}
           </Select>
 
-          <Select value={ currentAccount } style={{ width: 120 }}>
-            {currentAccounts.map(account => (
-              <Select.Option key={account.address} value={account.address}>{account.meta.name}</Select.Option>
+          <Select
+            value={currentAccount}
+            style={{ width: 120 }}
+          >
+            {currentAccounts.map((account) => (
+              <Select.Option
+                key={account.address}
+                value={account.address}
+              >
+                {account.meta.name}
+              </Select.Option>
             ))}
           </Select>
         </div>
       </div>
 
       <div className="content">
-        <Table columns={columns} dataSource={data} pagination={false} />
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={false}
+        />
       </div>
 
       <Modal
         title={`Create Plan for ${current}`}
         maskClosable={false}
         visible={createModalVisible}
-        onCancel={() => { setCreateModalVisible(false) }}
-        onOk={() => { }}
+        onCancel={() => {
+          setCreateModalVisible(false);
+        }}
+        onOk={() => {}}
       >
         <Form
           name="createPlanForm"
           form={form}
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 14 }}
-          initialValues={{ currency: '' }}
+          initialValues={{ currency: "" }}
         >
           <Form.Item
             label="Invest Amount"
             name="amount"
             rules={[
-              { required: true, message: 'Please input your currency!' },
+              {
+                required: true,
+                message:
+                  "Please input your currency!",
+              },
             ]}
           >
             <Input placeholder="Invest Amount" />
@@ -129,20 +225,34 @@ function AutoInvest() {
             label="Invest Currency"
             name="investCurrency"
             rules={[
-              { required: true, message: 'Please input your currency!' },
+              {
+                required: true,
+                message:
+                  "Please input your currency!",
+              },
             ]}
           >
             <Select placeholder="Invest Currency">
-              <Select.Option value="BTC">BTC</Select.Option>
-              <Select.Option value="ETH">ETH</Select.Option>
-              <Select.Option value="USDT">USDT</Select.Option>
+              <Select.Option value="BTC">
+                BTC
+              </Select.Option>
+              <Select.Option value="ETH">
+                ETH
+              </Select.Option>
+              <Select.Option value="USDT">
+                USDT
+              </Select.Option>
             </Select>
           </Form.Item>
           <Form.Item
             label="Buy Price"
             name="buyPrice"
             rules={[
-              { required: true, message: 'Please input your currency!' },
+              {
+                required: true,
+                message:
+                  "Please input your currency!",
+              },
             ]}
           >
             <Input placeholder="Buy Price" />
@@ -151,57 +261,93 @@ function AutoInvest() {
             label="Cycle Type"
             name="cycleType"
             rules={[
-              { required: true, message: 'Please input your currency!' },
+              {
+                required: true,
+                message:
+                  "Please input your currency!",
+              },
             ]}
           >
             <Radio.Group>
-              <Radio.Button value="day">Day</Radio.Button>
-              <Radio.Button value="week">Week</Radio.Button>
-              <Radio.Button value="month">Month</Radio.Button>
+              <Radio.Button value="day">
+                Day
+              </Radio.Button>
+              <Radio.Button value="week">
+                Week
+              </Radio.Button>
+              <Radio.Button value="month">
+                Month
+              </Radio.Button>
             </Radio.Group>
           </Form.Item>
           {({ getFieldValue }) => {
-            const cycleType = getFieldValue('cycleType')
-            if (cycleType === 'week') {
+            const cycleType =
+              getFieldValue("cycleType");
+            if (cycleType === "week") {
               return (
                 <Form.Item
                   label="Cycle Date"
                   name="cycleDate"
                   rules={[
-                    { required: true, message: 'Please input your currency!' },
+                    {
+                      required: true,
+                      message:
+                        "Please input your currency!",
+                    },
                   ]}
                 >
                   <Select placeholder="Cycle Date">
-                    <Select.Option value="1">1</Select.Option>
-                    <Select.Option value="2">2</Select.Option>
-                    <Select.Option value="3">3</Select.Option>
-                    <Select.Option value="4">4</Select.Option>
-                    <Select.Option value="5">5</Select.Option>
-                    <Select.Option value="6">6</Select.Option>
-                    <Select.Option value="7">7</Select.Option>
+                    <Select.Option value="1">
+                      1
+                    </Select.Option>
+                    <Select.Option value="2">
+                      2
+                    </Select.Option>
+                    <Select.Option value="3">
+                      3
+                    </Select.Option>
+                    <Select.Option value="4">
+                      4
+                    </Select.Option>
+                    <Select.Option value="5">
+                      5
+                    </Select.Option>
+                    <Select.Option value="6">
+                      6
+                    </Select.Option>
+                    <Select.Option value="7">
+                      7
+                    </Select.Option>
                   </Select>
                 </Form.Item>
-              )
-            }
-            else if (cycleType === 'month') {
+              );
+            } else if (cycleType === "month") {
               return (
                 <Form.Item
                   label="Cycle Date"
                   name="cycleDate"
                   rules={[
-                    { required: true, message: 'Please input your currency!' },
+                    {
+                      required: true,
+                      message:
+                        "Please input your currency!",
+                    },
                   ]}
                 >
                   <Input placeholder="1 - 28" />
                 </Form.Item>
-              )
+              );
             }
           }}
           <Form.Item
             label="Cycle Time"
             name="cycleTime"
             rules={[
-              { required: true, message: 'Please input your currency!' },
+              {
+                required: true,
+                message:
+                  "Please input your currency!",
+              },
             ]}
           >
             <TimePicker format="HH:mm" />
@@ -209,7 +355,7 @@ function AutoInvest() {
         </Form>
       </Modal>
     </div>
-  )
+  );
 }
 
-export default AutoInvest
+export default AutoInvest;
